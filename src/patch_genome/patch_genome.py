5251362ac8c5cfdf23825c8cc332007d08476fd2
Adam Diehl <adadiehl@umich.edu>
2024-10-24 15:46:25 -0400

    Removed some unused code and options relevant to the old way of infering contig breakpoints.

--- src/patch_genome/patch_genome.py
+++ src/patch_genome/patch_genome.py
@@ -11,38 +11,10 @@ __version__ = "0.0.1"
 
 """
 Given a name-sorted BAM file, cluster mapped reads by name and
-use the maximal interval defined by the primary and supplementary
-reads on the same chromosome and strand to define the contig
-break-points in the reference genome.
+use the maximal interval defined by the primary alignment's
+cigar string to infer the contig break-points in the reference 
+genome.
 """
-
-def create_alignment_dict(query_bam, min_qual_score, primary_only=False):
-    """
-    Create a dict of alignments, keyed on read name. This gives us
-    the clusters of primary and supplementary alignments we need to
-    define the BED intervals that bound contigs in the refernece
-    genome.
-    """
-    ret = {}    
-    for aln in query_bam:
-        if aln.is_unmapped:
-            # Skip unmapped contigs
-            continue
-        if aln.mapping_quality < min_qual_score:
-            # Skip low-quality mappings
-            continue
-        if aln.query_name in ret:
-            if primary_only and not (aln.is_supplementary or aln.is_secondary):
-                ret[aln.query_name].append(aln)
-            elif primary_only == False:
-                ret[aln.query_name].append(aln)
-        else:
-            if primary_only and not (aln.is_supplementary or aln.is_secondary):
-                ret[aln.query_name] = [aln]
-            elif primary_only == False:
-                ret[aln.query_name] = [aln]
-    return(ret)
-
 
 def create_primary_alignments_list(query_bam, min_qual_score):
     """
@@ -60,144 +32,6 @@ def create_primary_alignments_list(query_bam, min_qual_score):
         if not (aln.is_supplementary or aln.is_secondary):
             ret.append(aln)
     return ret
-
-
-def find_breakpoints(aligned_contigs, query_bam, max_merge_dist, min_mapped_fraction, max_expansion):
-    """
-    Deterimine maximal BED intervals for contig mapping chrom, start, and stop
-    based on the reference start and end coordinates of primary and supplementary
-    contig mappings.
-    """
-    contig_breakpoints = {}
-    primary_alignments = []
-    for cname in aligned_contigs:
-        #sys.stderr.write("%s\n" % (cname))
-        # First identify primary and supplementary alignments
-        supplementary_aln = []
-        to_keep = []
-        for aln in aligned_contigs[cname]:
-            if aln.is_supplementary or aln.is_secondary:
-                supplementary_aln.append(aln)
-            else:
-                # Primary alignment. Store as first index.
-                to_keep.append(aln)
-
-        # Skip contigs where the primary alignment is absent (due to
-        # not passing the quality filter, for example).
-        if len(to_keep) == 0:
-            continue
-
-        # Next, find all supplementary alignments on the same chromosome
-        # and strand as the primary alignment.
-        for aln in supplementary_aln:
-            if aln.reference_name == to_keep[0].reference_name:
-                if (aln.is_reverse and to_keep[0].is_reverse) or (not aln.is_reverse and not to_keep[0].is_reverse):
-                    to_keep.append(aln)
-        # Next, order the alignents by mapped position. This requires
-        # a BAM file intermediate.
-        sorted_bam_name = sorted_bam_from_aln_list(to_keep, query_bam.header)
-        pysam.index(sorted_bam_name)
-
-        # Read the position-sorted alignments into a list for
-        # further processing.
-        sorted_aln = []
-        sorted_bam = pysam.AlignmentFile(sorted_bam_name, "rb")
-        for aln in sorted_bam:
-            sorted_aln.append(aln)
-        sorted_bam.close()
-        os.remove(sorted_bam_name)
-        os.remove(sorted_bam_name + '.bai')
-
-        # Locate the primary alignment in the sorted list and check distances
-        # between supplementary alignments to exclude any that are excessively
-        # far away from the primary alignment site.
-        primary_idx = 0
-        for idx, aln in enumerate(sorted_aln):
-            if not aln.is_secondary and not aln.is_supplementary:
-                # Primary alignment
-                primary_idx = idx
-        final_aln = [sorted_aln[primary_idx]]
-        # Starting at the indeces one-off the primary alignment, check that the
-        # distance between upstream supplementary alignments is within the
-        # allowed range.
-        i = primary_idx - 1
-        prev_start = 0
-        offset = 0
-        while i >= 0:
-            # If multiple contigs share the same start coordinate, SAMtools
-            # sorts them based on ASCENDING end position, so the current alignment
-            # may not have the 3'-most end position. This can also happen with
-            # nested reads. In either case, we need to explicitly look for the
-            # 3'-most end position.
-            if i > 0 and sorted_aln[i-1].reference_end >= sorted_aln[i].reference_end:
-                #sys.stderr.write("%d\t%d\t%d\t%d\n" % (i, sorted_aln[i].reference_start, sorted_aln[i].reference_end, sorted_aln[i-1].reference_end))
-                i -= 1
-                offset += 1
-                continue
-            
-            dist = abs(sorted_aln[i+1+offset].reference_start - sorted_aln[i].reference_end)
-            #sys.stderr.write("%d\t%d\t%d\t%d\n" % (i, dist, sorted_aln[i+1+offset].reference_start, sorted_aln[i].reference_end))
-            if dist <= max_merge_dist:
-                final_aln.append(sorted_aln[i])
-                offset = 0
-            else:
-                # Since these are sorted by position, we can (and should!) stop looking after
-                # we find any distances greater than the threshold.
-                break
-            i -= 1
-        # Next check downstream alignments.
-        i = primary_idx + 1
-        offset = 0
-        while i < len(sorted_aln):
-            # Make sure we have the 3'-most end position of any overlapping alignments.
-            if i < len(sorted_aln)-1 and sorted_aln[i+1].reference_end <= sorted_aln[i].reference_end:
-                offset += 1
-                i += 1
-                continue
-            
-            dist = abs(sorted_aln[i].reference_start - sorted_aln[i-1-offset].reference_end)
-            if dist <= max_merge_dist:
-                final_aln.append(sorted_aln[i])
-                offset = 0
-            else:
-                break
-            i += 1
-
-        # Collect the reference starts and ends and sort to locate the 5'-most
-        # start and 3'-most end to define our insertion interval.
-        ref_starts = []
-        ref_ends = []
-        for aln in final_aln:
-            ref_starts.append(aln.reference_start)
-            ref_ends.append(aln.reference_end)
-        ref_starts.sort()
-        ref_ends.sort()
-
-        # Check that the aligned length represents at least the
-        # minimum fraction required (by default, at least 50% of the
-        # contig length must be encompassed by the interval.)
-        mapped_len = abs(ref_ends[-1]-ref_starts[0])
-        if (mapped_len / to_keep[0].query_length) <= min_mapped_fraction:
-            continue
-
-        # Also check that the mapped interval has not expanded in length
-        # by more than the max_expansion param.
-        if (mapped_len / to_keep[0].query_length) > max_expansion:
-            continue
-
-        # We now have what we need. Store the result.
-        """
-        sys.stderr.write("%s\t%d\t%d\t%s\t%d\t%d\n" % (to_keep[0].reference_name, ref_starts[0],
-                                                       ref_ends[-1], to_keep[0].query_name,
-                                                       to_keep[0].query_length, ref_ends[-1]-ref_starts[0]))
-        """
-        # Store the primary alignment for later processing.
-        primary_alignments.append(to_keep[0])
-
-        # Store the BED interval for the maximal contig mapping.
-        contig_breakpoints[to_keep[0].query_name]=[to_keep[0].reference_name, ref_starts[0], ref_ends[-1], to_keep[0].query_name]
-
-    return primary_alignments, contig_breakpoints
 
 
 def contig_breakpoints_from_cigar(primary_alignments):
@@ -328,15 +162,6 @@ def main():
     parser.add_argument('-m', '--min_qual_score', metavar='N', type=int,
                         required=False, default=30,
                         help='Minimum mapping quality score to retain an alignment. Default=30')
-    parser.add_argument('-d', '--max_merge_dist', metavar='N', type=int,
-                        required=False, default=250000,
-                        help='Maximum distance to merge adjacent alignments within a cluster. Default=250000')
-    parser.add_argument('-f', '--min_mapped_fraction', metavar='FLOAT', type=float,
-                        required=False, default=0.1,
-                        help='Minimum fraction of the contig length that must be encompassed by the mapped interval. Default=0.1')
-    parser.add_argument('-e', '--max_expansion', metavar='FLOAT', type=float,
-                        required=False, default=2.0,
-                        help='Maximum factor by which the mapped interval is allowed to expand relative to the contig length. I.e., 2.0 allows the mapped interval to be up to double the length of the contig. Default=2.0')
     parser.add_argument('-w', '--whitelist', metavar='PATH', type=str,
                         required=False, default=None,
                         help='Path to BED file containing whitelist regions: i.e., the inverse of blacklist regions. Supplying this will have the effect of excluding alignments that fall entirely within blacklist regions. Default=None')
@@ -347,13 +172,11 @@ def main():
     
     # Load up the BAM into a dict, keyed on contig name.
     sys.stderr.write("Loading alignments...\n")
-    #aligned_contigs = create_alignment_dict(query_bam, args.min_qual_score)
     primary_alignments = create_primary_alignments_list(query_bam, args.min_qual_score)
 
     # Determine insertion break-points for each contig based
-    # on primary and supplementary mappings.
+    # on the cigar string for the primary alignment.
     sys.stderr.write("Locating contig breakpoints...\n")
-    #primary_alignments, contig_breakpoints = find_breakpoints(aligned_contigs, query_bam, args.max_merge_dist, args.min_mapped_fraction, args.max_expansion)
     contig_breakpoints = contig_breakpoints_from_cigar(primary_alignments)
 
     # Sort the useful primary alignments by position via pysam sort.
@@ -482,6 +305,7 @@ def main():
         patched_fasta.write("%s\n" % (ref_seq.format("fasta")))
 
     # Close open file handles, etc.
+    query_bam.close()
     sorted_primary_alignments.close()
     patched_fasta.close()
     patches_bed.close()
