bd937a5e277c92f3d4c71fec1ee168952fdb02f3
Adam Diehl <adadiehl@umich.edu>
2026-01-14 11:03:30 -0500

    make asm mode for minimap a variable

--- scripts/patch_genome.sh
+++ scripts/patch_genome.sh
@@ -18,6 +18,7 @@ SCRIPTS_PATH=/data/projects/adadiehl/genome_patching/GPatch/scripts
 
 # Set this to the number of compute threads to use for mapping steps
 THREADS=24
+ASM_MODE="asm20"  # Available values are asm5, asm10, asm20
 
 CBREAK_MAXDIST=20000000
 CBREAK_MAXCDIST=100000
@@ -26,7 +27,7 @@ CBREAK_MINSIZE=1000000
 
 # Initial mapping of the assembly genome and first-round of patching.
 echo "Initial mapping to reference..."
-minimap2 -x asm20 -t $THREADS -a $REFERENCE_FASTA $ASSEMBLY_FASTA | samtools view -b - > $PREFIX.bam 2> $PREFIX.GPatch.err
+minimap2 -x $ASM_MODE -t $THREADS -a $REFERENCE_FASTA $ASSEMBLY_FASTA | samtools view -b - > $PREFIX.bam 2> $PREFIX.GPatch.err
 
 echo "Initial genome patching..."
 if [ "${WHITELIST}" != "" ]; then
@@ -43,7 +44,7 @@ printf "pre-break total patched genome size (bp): %d\n" $(grep -v ">" $PREFIX.pa
 
 # Alignment to reference and dot-plots.
 echo "Aligning patched genome to reference..."
-minimap2 -x asm20 -t $THREADS $REFERENCE_FASTA $PREFIX.patched.fasta > $PREFIX.patched.$REFERENCE_NAME.paf 2>> $PREFIX.GPatch.err
+minimap2 -x $ASM_MODE -t $THREADS $REFERENCE_FASTA $PREFIX.patched.fasta > $PREFIX.patched.$REFERENCE_NAME.paf 2>> $PREFIX.GPatch.err
 echo "Generating initial dot-plots..."
 $SCRIPTS_PATH/create_dotplots.Rscript $PREFIX.patched.$REFERENCE_NAME.paf $PREFIX $REFERENCE_NAME 2>> $PREFIX.GPatch.err
 
@@ -55,7 +56,7 @@ echo "Breaking contigs at suspicious rearrangement breakpoints..."
 $SCRIPTS_PATH/break_contigs.py -f $ASSEMBLY_FASTA -b $PREFIX.breakpoints.txt > $PREFIX.cbreak.fa 2>> $PREFIX.GPatch.err
 
 echo "Remapping to reference..."
-minimap2 -x asm20 -t $THREADS -a $REFERENCE_FASTA $PREFIX.cbreak.fa | samtools view -b - > $PREFIX.cbreak.bam 2>> $PREFIX.GPatch.err
+minimap2 -x $ASM_MODE -t $THREADS -a $REFERENCE_FASTA $PREFIX.cbreak.fa | samtools view -b - > $PREFIX.cbreak.bam 2>> $PREFIX.GPatch.err
 
 echo "Stage two genome patching..."
 if [ "${WHITELIST}" != "" ]; then
@@ -71,6 +72,6 @@ printf "post-break total patched genome size (bp): %d\n" $(grep -v ">" $PREFIX.c
 
 # Alignment to reference and dot-plots.
 echo "Mapping the final patched genome against the reference..."
-minimap2 -x asm20 -t $THREADS $REFERENCE_FASTA $PREFIX.cbreak.patched.fasta > $PREFIX.cbreak.patched.$REFERENCE_NAME.paf 2>> $PREFIX.GPatch.err
+minimap2 -x $ASM_MODE -t $THREADS $REFERENCE_FASTA $PREFIX.cbreak.patched.fasta > $PREFIX.cbreak.patched.$REFERENCE_NAME.paf 2>> $PREFIX.GPatch.err
 echo "Generating dot plots"
 $SCRIPTS_PATH/create_dotplots.Rscript $PREFIX.cbreak.patched.$REFERENCE_NAME.paf $PREFIX.cbreak $REFERENCE_NAME 2>> $PREFIX.GPatch.err
