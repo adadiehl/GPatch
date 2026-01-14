5fafeb61e243be3ce36a68404209a7077511ad0f
Adam Diehl <adadiehl@umich.edu>
2026-01-14 14:27:49 -0500

    Bump version: 0.3.8 → 0.4.0

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def main():
 
     metadata = dict(
         name="GPatch",
-        version="0.3.8",
+        version="0.4.0",
         author="Adam Diehl",
         author_email="adadiehl@umich.edu",
         description="Assemble contigs into a chromosome-scalse pseudo-assembly using alignments to a reference sequence. Download the github repository for helper scripts to automate GPatch workflows, identify and correct misjoins in the contig assembly, produce dot-plots of patched pseudoassemblies to a reference assembly, and generate chrom.sizes and liftover chains for patched pseudoassemblies.",
